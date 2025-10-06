d3bdd0c72da117cf2dbcce25af62c69b94679c39
carlaregina <carla.reginavasconcellos@gmail.com>
2025-10-06 13:17:09 -0300

    microservice de user e email

---
+++ user/user/src/main/java/com/ms/user/controller/UserController.java
@@ -0,0 +1,30 @@
+package com.ms.user.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ms.user.dtos.UserDTO;
+import com.ms.user.models.User;
+import com.ms.user.service.UserService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public ResponseEntity<User> addUser(@RequestBody @Valid UserDTO dto){
+        return ResponseEntity.status(HttpStatus.CREATED).body(userService.create(dto));
+    }
+}
---
+++ user/user/src/main/java/com/ms/user/service/UserService.java
@@ -0,0 +1,37 @@
+package com.ms.user.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Service;
+
+import com.ms.user.dtos.UserDTO;
+import com.ms.user.models.User;
+import com.ms.user.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final UserProducer userProducer;
+
+    public UserService(UserRepository userRepository, UserProducer userProducer) {
+        this.userRepository = userRepository;
+        this.userProducer = userProducer;
+    }
+
+    public User create(UserDTO userDto){
+        var user =  new User();
+        BeanUtils.copyProperties(userDto, user);
+        user =  userRepository.save(user);
+        userProducer.publishMessageEmail(user);
+        return user;
+    }
+
+    public User updateStatus(EmailStatusResponse emailStatusResponse){
+        Optional<User> user = userRepository.findById(emailStatusResponse.getUserId());
+        if(user.isEmpty()) return null;
+        user.get().setStatus(emailStatusResponse.getStatus());
+        return userRepository.save(user.get());
+    }
+}
